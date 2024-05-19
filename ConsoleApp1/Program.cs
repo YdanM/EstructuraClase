15cf013ba18b9c33ad2846c9f51f630635c79976
Hp_PC1 <Hp_PC1@VICTORUGO>
2024-05-19 18:21:12 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraClase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MiembrosDeLaComunidad miembros = new MiembrosDeLaComunidad();
+
+            Empleado empleado = new Empleado();
+
+            Estudiante estudiante = new Estudiante();
+            
+            ExAlumno exAlumno = new ExAlumno();
+
+            Docente docente = new Docente();   
+            
+            Administrativo administrativo = new Administrativo();
+
+            Administrador admin = new Administrador();
+
+            Maestro maestro = new Maestro();
+        }
+    }
+}
